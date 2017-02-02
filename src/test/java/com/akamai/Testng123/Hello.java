c65f370b7346008fb6f9f59412d2642f41473eee
Deepak Kumar <irsdeepak@gmail.com>
2017-02-02 18:27:21 +0530

    message

--- src/test/java/com/akamai/Testng123/Hello.java
+++ src/test/java/com/akamai/Testng123/Hello.java
@@ -1,5 +1,7 @@
 package com.akamai.Testng123;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Hello {
@@ -18,4 +20,15 @@ public class Hello {
 		System.out.println("This is 2st Program");
 	}
 
+	
+	@Test
+	public void Launchssss()
+	{
+		System.setProperty("webdriver.chrome.driver", "//Users//deekumar//Downloads//chromedriver");
+		WebDriver driver= new ChromeDriver();
+	
+		driver.get("https://gmail.com");
+		
+		
+	}
 }
