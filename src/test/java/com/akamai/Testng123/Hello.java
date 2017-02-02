003d8c287858170ff9e470b68cf95151337572d6
Deepak Kumar <irsdeepak@gmail.com>
2017-02-02 15:31:12 +0530

    message

---
+++ src/test/java/com/akamai/Testng123/Hello.java
@@ -0,0 +1,21 @@
+package com.akamai.Testng123;
+
+import org.testng.annotations.Test;
+
+public class Hello {
+	
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("This is 1st Program");
+	}
+
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("This is 2st Program");
+	}
+
+}
